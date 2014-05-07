8bf05371faeddd3faca32fd083afb8120c1442c8
mmerayo30@gmail.com <mmerayo30@gmail.com@4771008d-2c0f-59cf-8245-39c7f7fb9831>
2014-05-07 12:01:35 +0000

    Declarations to its own control

---
+++ trunk/Code/Subasta.Lib.UserControls/UcDeclarationSelector.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using StructureMap;
+using Subasta.Client.Common.Extensions;
+using Subasta.Client.Common.Game;
+using Subasta.Domain;
+using Subasta.Domain.Game;
+
+namespace Subasta.Lib.UserControls
+{
+	public partial class UcDeclarationSelector : UserControl,ICustomUserControl
+	{
+		private IUserInteractionManager _interactionManager;
+		private IGameSetHandler _gameSetHandler;
+
+		public UcDeclarationSelector()
+		{
+			InitializeComponent();
+		}
+
+		public void Initialize()
+		{
+			_interactionManager = ObjectFactory.GetInstance<IUserInteractionManager>();
+			_gameSetHandler = ObjectFactory.GetInstance<IGameSetHandler>();
+			_gameSetHandler.GameHandler.GameSaysCompleted += GameHandler_GameSaysCompleted;
+			_gameSetHandler.GameHandler.HumanPlayerDeclarationSelectionNeeded += GameHandler_HumanPlayerDeclarationSelectionNeeded;
+
+			EnableDeclarationsInteraction(false);
+		}
+
+		private void EnableDeclarationsInteraction(bool enable)
+		{
+			btnDeclarations.PerformSafely(x => x.Enabled = enable);
+			cmbDeclarations.PerformSafely(x => x.Enabled = enable);
+			grpDeclarations.PerformSafely(x => x.Visible = enable);
+			grpDeclarations.PerformSafely(x => x.BringToFront());
+
+		}
+
+		private void GameHandler_GameSaysCompleted(ISaysStatus status)
+		{
+			_interactionManager.Reset();
+		}
+
+
+		Declaration? GameHandler_HumanPlayerDeclarationSelectionNeeded(IHumanPlayer source, Domain.Declaration[] availableDeclarations, IExplorationStatus status)
+		{
+			var declarables = source.GetUserDeclarables(status).Where(availableDeclarations.Contains);
+			if (!declarables.Any())
+				return null;
+
+			LoadDeclarables(declarables.Cast<Declaration?>(), availableDeclarations.Length > declarables.Count());
+			EnableDeclarationsInteraction(true);
+			return _interactionManager.WaitUserInput<Declaration?>();
+
+		}
+
+		private void LoadDeclarables(IEnumerable<Declaration?> declarables, bool loadMateChooseOption)
+		{
+			var source = declarables.ToDictionary(value => value.ToString(), value => value.ToString().SeparateCamelCase());
+
+			if (loadMateChooseOption)
+				source.Add("Mate", "(Que cante el compañero)");
+
+			cmbDeclarations.PerformSafely(x => x.DataSource = new BindingSource(source, null));
+			cmbDeclarations.PerformSafely(x => x.ValueMember = "Key");
+			cmbDeclarations.PerformSafely(x => x.DisplayMember = "Value");
+		}
+
+		private void btnDeclarations_Click(object sender, EventArgs e)
+		{
+			_interactionManager.InputProvided(() =>
+			{
+				var selectedValue = (string)cmbDeclarations.SelectedValue;
+				var result = selectedValue == "Mate" ? null : (Declaration?)Enum.Parse(typeof(Declaration), selectedValue);
+				EnableDeclarationsInteraction(false);
+				return result;
+			});
+		}
+
+	}
+}
--- trunk/Code/Subasta.Lib/FrmGameSetInfo.cs
+++ trunk/Code/Subasta.Lib/FrmGameSetInfo.cs
@@ -16,23 +16,11 @@ namespace Subasta.Lib
 {
 	public partial class FrmGameSetInfo : Form
 	{
-		private readonly IGameSetHandler _gameSetHandler;
-		private readonly IUserInteractionManager _interactionManager;
-
-		public FrmGameSetInfo(IGameSetHandler gameSetHandler, IUserInteractionManager interactionManager)
+		public FrmGameSetInfo()
 		{
-			_gameSetHandler = gameSetHandler;
-			_interactionManager = interactionManager;
-
+			
 			InitializeComponent();
 			
-			//as copied from frmSays
-			_gameSetHandler.GameHandler.GameSaysCompleted += GameHandler_GameSaysCompleted;
-			_gameSetHandler.GameHandler.HumanPlayerDeclarationSelectionNeeded += GameHandler_HumanPlayerDeclarationSelectionNeeded;
-
-			EnableDeclarationsInteraction(false);
-
-
 			InitializeCustomUserControls();
 
 		}
@@ -45,81 +33,6 @@ namespace Subasta.Lib
 				userControl.Initialize();
 			}
 		}
-
-		#region Brought from frmSays
-
-		private void GameHandler_GameSaysCompleted(ISaysStatus status)
-		{
-			_interactionManager.Reset();
-		}
-
-
-		Declaration? GameHandler_HumanPlayerDeclarationSelectionNeeded(IHumanPlayer source, Domain.Declaration[] availableDeclarations, IExplorationStatus status)
-		{
-			var declarables = source.GetUserDeclarables(status).Where(availableDeclarations.Contains);
-			if (!declarables.Any())
-				return null;
-
-			LoadDeclarables(declarables.Cast<Declaration?>(), availableDeclarations.Length > declarables.Count());
-			EnableDeclarationsInteraction(true);
-			return _interactionManager.WaitUserInput<Declaration?>();
-
-		}
-
-		private void LoadDeclarables(IEnumerable<Declaration?> declarables, bool loadMateChooseOption)
-		{
-			var source = declarables.ToDictionary(value => value.ToString(), value => value.ToString().SeparateCamelCase());
-
-			if (loadMateChooseOption)
-				source.Add("Mate", "(Que cante el compañero)");
-
-			cmbDeclarations.PerformSafely(x => x.DataSource = new BindingSource(source, null));
-			cmbDeclarations.PerformSafely(x => x.ValueMember = "Key");
-			cmbDeclarations.PerformSafely(x => x.DisplayMember = "Value");
-		}
-
-
-
-		
-
-		
-
-		
-
-		
-
-		
-
-		
-
-		private void btnDeclarations_Click(object sender, EventArgs e)
-		{
-			_interactionManager.InputProvided(() =>
-			{
-				var selectedValue = (string)cmbDeclarations.SelectedValue;
-				var result = selectedValue == "Mate" ? null : (Declaration?)Enum.Parse(typeof(Declaration), selectedValue);
-				EnableDeclarationsInteraction(false);
-				return result;
-			});
-		}
-
-		
-
-
-		private void EnableDeclarationsInteraction(bool enable)
-		{
-			btnDeclarations.PerformSafely(x => x.Enabled = enable);
-			cmbDeclarations.PerformSafely(x => x.Enabled = enable);
-			grpDeclarations.PerformSafely(x => x.Visible = enable);
-			grpDeclarations.PerformSafely(x => x.BringToFront());
-			
-		}
-
-		#endregion
-
-		
-
-		
 
 		
 	}
